09cdad42a0bca469a687adc28e344714caa50b42
Gabriel <grrodrigues.devr@gmail.com>
2021-10-03 11:57:39 -0300

    Criando um catálogo de jofos usando boas prática de arquitetura com .NET

---
+++ Controllers/V1/JogosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+    }
+}
